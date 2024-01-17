7fd731c6ed8b13ef057f97c6bda1ea6ba24446ae
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-17 18:33:26 +0530

    Calculates Mathematical operationS

---
+++ Arithmetic Operations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a,b,w,x,y,z;
+	a=50,b=30;
+	w=a+b;
+	x=a-b;
+	y=a*b;
+	z=a/b;
+	printf("sum: %d",w);
+	printf("\nSubstraction: %d",x);
+	printf("\nProduct: %d",y);
+	printf("\nDivision: %d",z);
+	return 0;	
+}
