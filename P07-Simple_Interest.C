e32f8ca5d2067147b5a94673359be81cd65af013
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:42:35 +0530

    Update P07-Simple_Interest.C Calculates simple interest based on principal amount, rate, and time.

--- P07-Simple_Interest.C
+++ P07-Simple_Interest.C
@@ -1,13 +1,9 @@
-// P07-Simple_Interest.C
-// Description: Calculates simple interest based on principal amount, rate, and time.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables for principal amount (p), rate (r), time (t), and simple interest (I)
     float p, r, t, I;
 
-    // Get user input for principal amount, rate, and time
     printf("Enter the principal amount (p): ");
     scanf("%f", &p);
     printf("Enter the rate (r): ");
@@ -15,12 +11,9 @@ int main(){
     printf("Enter the time (t): ");
     scanf("%f", &t);
 
-    // Calculate simple interest using the formula: I = (p * r * t) / 100
     I = (p * r * t) / 100;
 
-    // Print the calculated simple interest
     printf("Simple interest: %f", I);
 
-    // Return 0 to indicate successful execution
     return 0;
 }
