25088c2bc190a0fdb0d7225ebcc0862103d7c0d5
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:43:12 +0530

    Update P08-Days_to_YMD.CConverts a given number of days into years, months, and days.

--- P08-Days_to_YMD.C
+++ P08-Days_to_YMD.C
@@ -1,24 +1,17 @@
-// P08-Days_to_YMD.C
-// Description: Converts a given number of days into years, months, and days.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables for input value (x), years (year), months (month), and days (day)
     int x, year, month, day;
 
-    // Get user input for the number of days
     printf("Enter the value: ");
     scanf("%d", &x);
 
-    // Calculate years, months, and days
     year = x / 365;
     month = (x % 365) / 30;
     day = (x % 365) % 30;
 
-    // Print the result
     printf("Years: %d\nMonths: %d\nDays: %d", year, month, day);
 
-    // Return 0 to indicate successful execution
     return 0;
 }
