d2d23f2dd19170854e166feb4778efdbe995c24a
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:29:14 +0530

    Update P20-Triangle_Validity_Check.C Checks the validity of a triangle based on the lengths of its three sides.

--- P20-Triangle_Validity_Check.C
+++ P20-Triangle_Validity_Check.C
@@ -1,17 +1,12 @@
-// P20-Triangle_Validity_Check.C
-// Description: Checks the validity of a triangle based on the lengths of its three sides.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables for three sides of the triangle (a, b, c)
     int a, b, c;
 
-    // Get user input for three sides of the triangle
     printf("Enter three sides of the triangle: ");
     scanf("%d%d%d", &a, &b, &c);
 
-    // Check the conditions for the validity of a triangle
     if (a + b > c && a + c > b && b + c > a) {
         printf("Valid triangle");
     }
@@ -19,6 +14,5 @@ int main(){
         printf("Invalid triangle");
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
