3e473b53273305c30216aa1ed635f64eca186818
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:33:30 +0530

    Update P25-Point_Position_Check.CChecks the position of a point (x, y) relative to the x and y axes.

--- P25-Point_Position_Check.C
+++ P25-Point_Position_Check.C
@@ -1,17 +1,12 @@
-// P25-Point_Position_Check.C
-// Description: Checks the position of a point (x, y) relative to the x and y axes.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables for coordinates of a point (x, y)
     int x, y;
 
-    // Get user input for the coordinates of the point (x, y)
     printf("Enter the coordinates (x, y): ");
     scanf("%d%d", &x, &y);
 
-    // Check the position of the point relative to the x and y axes
     if (x == 0 && y == 0) {
         printf("The point is at the origin");
     }
@@ -25,6 +20,5 @@ int main(){
         printf("The point is not on the x or y axis");
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
