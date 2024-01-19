5ed8b934ccdb5e2d8d7a062e0d84f85c0ac94cf2
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:38:30 +0530

    Update P02-Arithmetic_Operations.CPerforms basic arithmetic operations (sum, subtraction, product, division) on two integers.

--- P02-Arithmetic_Operations.C
+++ P02-Arithmetic_Operations.C
@@ -1,28 +1,21 @@
-// P02-Arithmetic_Operations.C
-// Description: Performs basic arithmetic operations (sum, subtraction, product, division) on two integers.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables
     int a, b, w, x, y, z;
 
-    // Assign values to variables
     a = 50;
     b = 30;
 
-    // Calculate sum, subtraction, product, and division
     w = a + b;
     x = a - b;
     y = a * b;
     z = a / b;
 
-    // Print results
     printf("Sum: %d\n", w);
     printf("Subtraction: %d\n", x);
     printf("Product: %d\n", y);
     printf("Division: %d\n", z);
 
-    // Return 0 to indicate successful execution
     return 0;
 }
