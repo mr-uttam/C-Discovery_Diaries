48289efd8a8a0eb593c8a5ec3123e809fc9004f7
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:28:16 +0530

    Update P19-Greater_Number.CDetermines the greater number among three random input numbers.

--- P19-Greater_Number.C
+++ P19-Greater_Number.C
@@ -1,17 +1,12 @@
-// P19-Greater_Number.C
-// Description: Determines the greater number among three random input numbers.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables for three random numbers (a, b, c)
     int a, b, c;
 
-    // Get user input for three random numbers
     printf("Enter three random numbers: ");
     scanf("%d%d%d", &a, &b, &c);
 
-    // Check conditions to find the greater number
     if (a > b && a > c) {
         printf("The greater number is %d", a);
     }
@@ -22,6 +17,5 @@ int main(){
         printf("The greater number is %d", c);
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
