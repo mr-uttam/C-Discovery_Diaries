62965a0546a8d35fea58032a19fcb2ccfa20ab38
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:30:23 +0530

    Update P21-Youngest_Boy.C Determines the youngest boy among Ram, Shyam, and Madhu based on their ages.

--- P21-Youngest_Boy.C
+++ P21-Youngest_Boy.C
@@ -1,25 +1,18 @@
-// P21-Youngest_Boy.C
-// Description: Determines the youngest boy among Ram, Shyam, and Madhu based on their ages.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables for the ages of Ram (a), Shyam (b), and Madhu (c)
     int a, b, c;
 
-    // Get user input for the age of Ram
     printf("The age of Ram: ");
     scanf("%d", &a);
 
-    // Get user input for the age of Shyam
     printf("The age of Shyam: ");
     scanf("%d", &b);
 
-    // Get user input for the age of Madhu
     printf("The age of Madhu: ");
     scanf("%d", &c);
 
-    // Check conditions to find the youngest boy
     if (a < b && a < c) {
         printf("The youngest boy is Ram");
     }
@@ -30,6 +23,5 @@ int main(){
         printf("The youngest boy is Madhu");
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
