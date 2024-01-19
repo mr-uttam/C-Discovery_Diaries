25ce95e18b72484176bd53b0d5a97122bd340a05
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:39:16 +0530

    Update P03-Floating_Point_Arithmetic.CPerforms basic arithmetic operations (sum, subtraction, product, division) on two floating-point numbers.

--- P03-Floating_Point_Arithmetic.C
+++ P03-Floating_Point_Arithmetic.C
@@ -1,32 +1,24 @@
-// P03-Floating_Point_Arithmetic.C
-// Description: Performs basic arithmetic operations (sum, subtraction, product, division) on two floating-point numbers.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables
     float a, b, w, x, y, z;
 
-    // Get user input for the first number
     printf("\nEnter the first number: ");
     scanf("%f", &a);
 
-    // Get user input for the second number
     printf("Enter the second number: ");
     scanf("%f", &b);
 
-    // Calculate sum, subtraction, product, and division
     w = a + b;
     x = a - b;
     y = a * b;
     z = a / b;
 
-    // Print results
     printf("The sum is: %f\n", w);
     printf("The subtraction is: %f\n", x);
     printf("The product is: %f\n", y);
     printf("The division is: %f\n", z);
 
-    // Return 0 to indicate successful execution
     return 0;
 }
