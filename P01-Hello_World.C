948e0c4866a3ee353580f61b46c5c1b0956cd2c7
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-19 21:37:44 +0530

    Update P01-Hello_World.CSimple C program to print "Hello World" to the console.

--- P01-Hello_World.C
+++ P01-Hello_World.C
@@ -1,15 +1,8 @@
-// P01-Hello_World.C
-// Description: Simple C program to print "Hello World" to the console.
 
 #include<stdio.h>
 
 int main(){
-    // Print the greeting message
     printf("Hello World");
 
-    // Return 0 to indicate successful execution
     return 0;
 }
-```
-
-Feel free to share more C codes for renaming and commenting.
