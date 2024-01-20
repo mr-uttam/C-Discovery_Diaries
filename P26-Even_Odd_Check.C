ff7725e4eec8336e59933e993648824c29e2e0f5
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 16:39:45 +0530

    Checks whether a given number is even or odd.

--- P26-Even_Odd_Check.C
+++ P26-Even_Odd_Check.C
@@ -1,17 +1,12 @@
-// P26-Even_Odd_Check.C
-// Description: Checks whether a given number is even or odd.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variable for user input (n)
     int n;
 
-    // Get user input for a number
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    // Check if the number is even or odd
     if (n % 2 == 0) {
         printf("The number is even");
     }
@@ -19,6 +14,5 @@ int main(){
         printf("The number is odd");
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
