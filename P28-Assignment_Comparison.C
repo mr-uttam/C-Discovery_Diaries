69c6da64f18c9fa5212f9377492b69c34c2ecbdf
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 16:41:25 +0530

    Demonstrates variable assignments and comparisons in C.

--- P28-Assignment_Comparison.C
+++ P28-Assignment_Comparison.C
@@ -1,19 +1,13 @@
-// P28-Assignment_Comparison.C
-// Description: Demonstrates variable assignments and comparisons in C.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variables x, y, z
     int x = 3, y, z;
 
-    // Perform assignments and comparison
-    y = x = 10;  // Assigns 10 to x, then assigns the value of x to y
-    z = x < 10;  // Compares if x is less than 10 and assigns the result to z
+    y = x = 10;  
+    z = x < 10;  
 
-    // Print the values of x, y, and z
     printf("\nx=%d, y=%d, z=%d", x, y, z);
 
-    // Return 0 to indicate successful execution
     return 0;
 }
