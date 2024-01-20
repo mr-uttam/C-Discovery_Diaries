9a0827eb26879c6b20556dd52de41ac49401bd97
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 16:40:35 +0530

    Checks whether a given number is even or odd using the ternary operator.

--- P27-Even_Odd_Check_Ternary.C
+++ P27-Even_Odd_Check_Ternary.C
@@ -1,19 +1,13 @@
-// P27-Even_Odd_Check_Ternary.C
-// Description: Checks whether a given number is even or odd using the ternary operator.
 
 #include<stdio.h>
 
 int main(){
-    // Declare variable for user input (n)
     int n;
 
-    // Get user input for a number
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    // Use ternary operator to check if the number is even or odd
     n % 2 == 0 ? printf("The number is even") : printf("The number is odd");
 
-    // Return 0 to indicate successful execution
     return 0;
 }
