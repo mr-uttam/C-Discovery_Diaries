3e77f8c5eb8790cf7e97a729754c5044faed43c0
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 17:53:07 +0530

    Prints the digits of a user-input number in reverse order.

---
+++ P48-Print_Digits_in_Reverse_Order.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        printf("%d", n % 10);
+
+        n /= 10;
+    }
+
+    return 0;
+}
