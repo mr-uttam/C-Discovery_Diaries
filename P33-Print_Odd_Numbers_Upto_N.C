89ed340ac01874f1ace7a250a8ddc5e066a07bbf
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 16:58:10 +0530

    Prints odd numbers from 1 to the user-input last number (n).

--- P33-Print_Odd_Numbers_Upto_N.C
+++ P33-Print_Odd_Numbers_Upto_N.C
@@ -1,21 +1,14 @@
-// P33-Print_Odd_Numbers_Upto_N.C
-// Description: Prints odd numbers from 1 to the user-input last number (n).
-
 #include<stdio.h>
 
 int main(){
-    // Declare variables for loop control (i) and user input (n)
     int i, n;
 
-    // Get user input for the number
     printf("Enter the number: ");
     scanf("%d", &n);
 
-    // Use a loop to print odd numbers from 1 to n
     for(i = 1; i <= n; i = i + 2){
         printf("%d\n", i);
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
