ec48d07c28242deab413211da5b540b264423bc1
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 17:04:35 +0530

    Checks whether a given number is prime or composite.

--- P39-Prime_or_Composite.C
+++ P39-Prime_or_Composite.C
@@ -1,26 +1,18 @@
-// P39-Prime_or_Composite.C
-// Description: Checks whether a given number is prime or composite.
-
 #include<stdio.h>
 
 int main(){
-    // Declare variables for loop control (i), user input (n), and a flag for prime (a)
     int i, n, a = 0;
 
-    // Get user input for the number
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    // Use a loop to check for factors of the number
     for(i = 2; i <= n - 1; i++){
-        // If a factor is found, set the flag to indicate the number is composite
         if(n % i == 0){
             a = 1;
             break;
         }
     }
 
-    // Check the flag to determine whether the number is prime or composite
     if(a == 0){
         printf("The number is prime");
     }
@@ -28,6 +20,5 @@ int main(){
         printf("The number is composite");
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
