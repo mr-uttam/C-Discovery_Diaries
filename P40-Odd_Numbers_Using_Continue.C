84fee0de4909c7cb50c56dca4889ac53b60b1011
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 17:05:32 +0530

    Prints odd numbers up to a given number using the 'continue' statement.

--- P40-Odd_Numbers_Using_Continue.C
+++ P40-Odd_Numbers_Using_Continue.C
@@ -1,27 +1,18 @@
-// P40-Odd_Numbers_Using_Continue.C
-// Description: Prints odd numbers up to a given number using the 'continue' statement.
-
 #include<stdio.h>
 
 int main(){
-    // Declare variables for loop control (i) and user input (n)
     int i, n;
 
-    // Get user input for the number
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    // Use a loop to print odd numbers up to the given number using 'continue'
     for(i = 1; i <= n; i++){
-        // Skip even numbers using 'continue'
         if(i % 2 == 0){
             continue;
         }
 
-        // Print the current odd number
         printf("%d  ", i);
     }
 
-    // Return 0 to indicate successful execution
     return 0;
 }
