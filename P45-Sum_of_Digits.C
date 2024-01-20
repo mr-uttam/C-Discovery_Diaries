c939e6b2bf5e98a0120d22eac4606a95e32d2d0b
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-20 17:24:06 +0530

    Calculates the sum of digits in a user-input number.

---
+++ P45-Sum_of_Digits.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int x = 0;
+
+    while(n != 0){
+        int a = n % 10;
+
+        n = n / 10;
+
+        x = x + a;
+    }
+
+    printf("%d", x);
+
+    return 0;
+}
