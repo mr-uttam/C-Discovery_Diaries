ce623fb85445bcca44848ba9bbdcb46216ed2c46
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-22 19:16:54 +0530

    Prints an inverted pattern of stars based on user-input rows.

---
+++ P67-Inverted_Star_Pattern.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j >= i; j--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
