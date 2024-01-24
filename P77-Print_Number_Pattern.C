6a90ec5489ce894f9c09450c8e32bfb8a3dc98e1
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-01-24 18:31:56 +0530

    Prints a number pattern in increasing order.

---
+++ P77-Print_Number_Pattern.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int a = 1;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d ", a);
+            a++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
