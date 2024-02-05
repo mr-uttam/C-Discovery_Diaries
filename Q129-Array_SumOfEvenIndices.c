1eb0c3096b2bebc9b1bac92a457eb3e78c82b408
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-02-05 19:16:54 +0530

    Calculates and prints the sum of elements at even indices in an array.

---
+++ Q129-Array_SumOfEvenIndices.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+    int i, n, sum = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            sum += arr[i];
+        }
+    }
+
+    printf("Sum of elements at even indices is %d", sum);
+
+    return 0;
+}
