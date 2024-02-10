970543b9af441bd341494c58605a5ac7b43392f0
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-02-10 22:56:41 +0530

    Takes user input to create a matrix,

---
+++ Q161-Line_Wave_of_Matrix_User_Input.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    // Initialize variables for matrix dimensions
+    int m, n;
+
+    // Get dimensions and elements of the matrix from the user
+    printf("Enter rows of the matrix: ");
+    scanf("%d", &m);
+    printf("Enter columns of the matrix: ");
+    scanf("%d", &n);
+    printf("Enter elements of the matrix:\n\n");
+    int arr[m][n];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // Display the entered matrix
+    printf("Entered Matrix:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Display the lines of the matrix in a wave pattern
+    printf("\nLine wave of the matrix is:\n");
+    for (int j = 0; j < n; j++) {
+        if (j % 2 == 0) {
+            // Print the line in reverse order
+            for (int i = m - 1; i >= 0; i--) {
+                printf("%d ", arr[i][j]);
+            }
+        } else {
+            // Print the line in the original order
+            for (int i = 0; i < m; i++) {
+                printf("%d ", arr[i][j]);
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
