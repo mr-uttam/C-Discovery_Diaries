63b94cbd6e6026487b5272deb2604ab77bf5aae2
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-02-20 19:04:56 +0530

    Initializes a string using a pointer, calculates its length using strlen(),

---
+++ Q166-String_Length_Using_Strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Initialize a string using a pointer
+    char *str = "Uttam Pal";
+
+    // Calculate the length of the string using strlen()
+    int x = strlen(str);
+
+    // Display the length of the string
+    printf("Length of the string: %d", x);
+
+    return 0;
+}
